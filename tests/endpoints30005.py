3585d7058daa4584f81b7421ad70515d8997fa0b
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2023-03-28 14:24:56 +0300

    Close to done endpoints on port 30005

--- tests/endpoints30005.py
+++ tests/endpoints30005.py
@@ -55,10 +55,105 @@ def test_test_name_returns_200():
     assert response.status_code == 200
 
 
-def test_test_returns_correct_body():
+def test_test_name_returns_correct_body():
     response = requests.get(url + "/test/john")
     body = ""
     if response.status_code == 200:
         body = response.content.decode("utf-8")
     assert "Hello john!" in body
 
+
+def test_metainfo_primary_key_returns_200():
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/metainfo/" + table + "/primarykey")
+        if response.status_code != 200:
+            errors.append("The endpoint " + url + "/metainfo/" + table + "/primarykey returned status code different then 200.")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
+
+def test_metainfo_primary_key_returns_correct_body():
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/metainfo/" + table + "/primarykey")
+        body = json.loads(response.content.decode("utf-8"))
+        if body != primary_keys()[table]:
+            errors.append("The endpoint " + url + "/metainfo/" + table + "/primarykey returned wrong body.")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
+
+def test_metainfo_primary_key_returns_correct_schema():
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/metainfo/" + table + "/primarykey")
+        body = json.loads(response.content.decode("utf-8"))
+        if not check(metainfo_primarykey_columns_schema(), body):
+            errors.append("The endpoint " + url + "/metainfo/" + table + "/primarykey doesn't match the schema.")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
+
+def test_metainfo_columns_returns_200():
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/metainfo/" + table + "/columns")
+        if response.status_code != 200:
+            errors.append("The endpoint " + url + "/metainfo/" + table + "/columns returned status code different then 200.")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
+
+def test_metainfo_columns_returns_correct_body():
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/metainfo/" + table + "/columns")
+        body = json.loads(response.content.decode("utf-8"))
+        if body != columns()[table]:
+            errors.append("The endpoint " + url + "/metainfo/" + table + "/columns returned wrong body.")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
+
+def test_metainfo_columns_returns_correct_schema():
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/metainfo/" + table + "/columns")
+        body = json.loads(response.content.decode("utf-8"))
+        if not check(metainfo_primarykey_columns_schema(), body):
+            errors.append("The endpoint " + url + "/metainfo/" + table + "/columns doesn't match the schema.")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
+
+def test_metainfo_indexes_returns_200():
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/metainfo/" + table + "/indexes")
+        if response.status_code != 200:
+            errors.append("The endpoint " + url + "/metainfo/" + table + "/indexes returned status code different then 200.")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
+
+def test_metainfo_indexes_returns_correct_body():
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/metainfo/" + table + "/indexes")
+        body = json.loads(response.content.decode("utf-8"))
+        if body != indexes()[table]:
+            errors.append("The endpoint " + url + "/metainfo/" + table + "/indexes returned wrong body.")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
+
+
+def test_metainfo_indexes_returns_correct_schema():
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/metainfo/" + table + "/indexes")
+        body = json.loads(response.content.decode("utf-8"))
+        if not check(metainfo_indexes_schema(), body):
+            errors.append("The endpoint " + url + "/metainfo/" + table + "/indexes doesn't match the schema.")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
