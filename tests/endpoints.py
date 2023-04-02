4213a9581973ccf083e783c411cbbc6575634c24
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2023-04-02 07:58:25 +0300

    Done the logging part

--- tests/endpoints.py
+++ tests/endpoints.py
@@ -2,8 +2,10 @@ import random
 import requests
 from data_requests.ihelp_request import *
 from data_requests.Schemas import *
+import logging
 
 url = "http://147.102.230.182:30007/ihelp/"
+logging.basicConfig(filename='endpoints30007.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
 def t_datacapture_request():
@@ -17,30 +19,40 @@ def t_datacapture_combo_request():
 def test_datacapture_test_returns_200():
     response = requests.get(url + "datacapture/test")
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint datacapture/test returned wrong status code: {status_code}')
     assert status_code == 200
 
 
 def test_datacapture_test_returns_correct_body():
     response = requests.get(url + "datacapture/test")
     body = response.content.decode("utf-8")
+    if "Hello from the test resource!" not in body:
+        logging.error(f'The endpoint datacapture/test returned wrong body: {body}')
     assert "Hello from the test resource!" in body
 
 
 def test_datacapture_get_returns_200():
     response = requests.get(url + "datacapture")
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint datacapture returned wrong status code: {status_code}')
     assert status_code == 200
 
 
 def test_datacapture_get_returns_correct_body():
     response = requests.get(url + "datacapture")
     body = response.content.decode("utf-8")
+    if not check(datacapture_list_schema(), body):
+        logging.error(f'The endpoint datacapture returned wrong wrong body: {body}')
     assert check(datacapture_list_schema(), body)
 
 
 def test_datacapture_post_returns_200():
     response = requests.post(url + "datacapture", json=t_datacapture_request())
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint POST datacapture returned wrong status code: {status_code}')
     assert status_code == 200
 
 
@@ -48,6 +60,8 @@ def test_datacapture_id_returns_200():
     my_id = random.randint(0, 100)
     response = requests.get(url + "datacapture/" + str(my_id))
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint datacapture/id returned wrong status code: {status_code}')
     assert status_code == 200
 
 
@@ -55,36 +69,48 @@ def test_datacapture_id_returns_correct_body():
     my_id = random.randint(0, 100)
     response = requests.get(url + "datacapture/" + str(my_id))
     body = response.content.decode("utf-8")
+    if not check(datacapture_schema(), body):
+        logging.error(f'The endpoint datacapture/id returned wrong wrong body: {body}')
     assert check(datacapture_schema(), body)
 
 
 def test_datacapture_combo_test_returns_200():
     response = requests.get(url + "datacapture/combo/test")
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint datacapture/combo_test returned wrong status code: {status_code}')
     assert status_code == 200
 
 
 def test_datacapture_combo_test_returns_correct_body():
     response = requests.get(url + "datacapture/combo/test")
     body = response.content.decode("utf-8")
+    if "Hello from the test resource!" not in body:
+        logging.error(f'The endpoint datacapture returned wrong body: {body}')
     assert "Hello from the test resource!" in body
 
 
 def test_datacapture_combo_returns_200():
     response = requests.get(url + "datacapture/combo")
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint datacapture/combo returned wrong status code: {status_code}')
     assert status_code == 200
 
 
 def test_datacapture_combo_returns_correct_body():
     response = requests.get(url + "datacapture/combo")
     body = response.content.decode("utf-8")
+    if not check(datacapture_combo_list_schema(), body):
+        logging.error(f'The endpoint datacapture/combo returned wrong wrong body: {body}')
     assert check(datacapture_combo_list_schema(), body)
 
 
 def test_datacapture_combo_post_returns_200():
     response = requests.post(url + "datacapture/combo", json=t_datacapture_combo_request())
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint POST datacapture/combo returned wrong status code: {status_code}')
     assert status_code == 200
 
 
@@ -92,6 +118,8 @@ def test_datacapture_combo_id_returns_200():
     my_id = random.randint(0, 100)
     response = requests.get(url + "datacapture/combo" + str(my_id))
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint datacapture/combo/id returned wrong status code: {status_code}')
     assert status_code == 200
 
 
@@ -99,28 +127,38 @@ def test_datacapture_combo_id_returns_correct_body():
     my_id = random.randint(0, 100)
     response = requests.get(url + "datacapture/combo" + str(my_id))
     body = response.content.decode("utf-8")
+    if not check(datacapture_combo_schema(), body):
+        logging.error(f'The endpoint datacapture/combo/id returned wrong wrong body: {body}')
     assert check(datacapture_combo_schema(), body)
 
 
 def test_datacapture_combo_schedule_test_returns_200():
     response = requests.get(url + "datacapture/combo/schedule/test")
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint datacapture/combo/schedule/test returned wrong status code: {status_code}')
     assert status_code == 200
 
 
 def test_datacapture_combo_schedule_test_returns_correct_body():
     response = requests.get(url + "datacapture/combo/schedule/test")
     body = response.content.decode("utf-8")
+    if "Hello from the test resource!" not in body:
+        logging.error(f'The endpoint datacapture/combo/schedule/test returned wrong body: {body}')
     assert "Hello from the test resource!" in body
 
 
 def test_datacapture_combo_schedule_returns_200():
     response = requests.get(url + "datacapture/combo/schedule")
     status_code = response.status_code
+    if status_code != 200:
+        logging.error(f'The endpoint datacapture/combo/schedule returned wrong status code: {status_code}')
     assert status_code == 200
 
 
 def test_datacapture_combo_schedule_returns_correct_body():
     response = requests.get(url + "datacapture/combo/schedule")
     body = response.content.decode("utf-8")
+    if not check(datacapture_combo_schedule_list_schema(), body):
+        logging.error(f'The endpoint datacapture/combo/schedule returned wrong wrong body: {body}')
     assert check(datacapture_combo_schedule_list_schema(), body)
--- tests/endpoints30005.py
+++ tests/endpoints30005.py
@@ -1,13 +1,17 @@
 import requests
 from data_requests.Schemas import *
 from data_requests.ihelp_request import *
+import logging
 
 url = "http://147.102.230.182:30005/ihelp"
 headers = {"Content-Type": "application/json; charset=utf-8"}
+logging.basicConfig(filename='endpoints30005.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
 def test_metainfo_returns_200():
     response = requests.get(url + "/metainfo")
+    if response.status_code != 200:
+        logging.error(f'The endpoint metainfo returned wrong status code: {response.status_code}')
     assert response.status_code == 200
 
 
@@ -16,29 +20,45 @@ def test_metainfo_returns_correct_body():
     body = ""
     if response.status_code == 200:
         body = response.content.decode("utf-8")
+    if body != '["ENCOUNTER","MEDICATION","CONDITION","IHELP_PERSON","PATIENT","FPG_BASE","FPG_BLOOD","HEALTHENTIA_SUBJECTS","HEALTHENTIA_ANSWERS","HEALTHENTIA_EXERCISES","HEALTHENTIA_PHYSIOLOGICAL"]':
+        logging.error(f'The endpoint metainfo returned wrong body: {body}')
     assert body == '["ENCOUNTER","MEDICATION","CONDITION","IHELP_PERSON","PATIENT","FPG_BASE","FPG_BLOOD","HEALTHENTIA_SUBJECTS","HEALTHENTIA_ANSWERS","HEALTHENTIA_EXERCISES","HEALTHENTIA_PHYSIOLOGICAL"]'
 
 
 def test_process_returns_200():
-    response = requests.get(url + "/process?query=something")
-    assert response.status_code == 200
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/process?query=SELECT * FROM " + table)
+        if response.status_code != 200:
+            errors.append(url + "/process?query=SELECT * FROM " + table + " returned status code: " + str(response.status_code))
+    if errors:
+        logging.error(f'For endpoint process the following errors for the status code occurred: {errors}')
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
 def test_process_returns_correct_body():
-    response = requests.get(url + "/process?query=something")
-    body = ""
-    if response.status_code == 200:
+    errors = []
+    for table in tables():
+        response = requests.get(url + "/process?query=SELECT * FROM " + table)
         body = response.content.decode("utf-8")
-    assert check(process_schema(), body)
+        if check(process_schema(), body):
+            errors.append(url + "/process?query=SELECT * FROM " + table + " returned wrong body")
+    if errors:
+        logging.error(f'For endpoint process the following errors for the body occurred: {errors}')
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
 def test_process_post_returns_200():
     response = requests.post(url + "/process", headers=headers, json=process_post_request(1, "male", "25-03-2023", "22", "22"))
+    if response.status_code != 200:
+        logging.error(f'The endpoint POST process returned wrong status code: {response.status_code}')
     assert response.status_code == 200
 
 
 def test_test_returns_200():
     response = requests.get(url + "/test")
+    if response.status_code != 200:
+        logging.error(f'The endpoint test returned wrong status code: {response.status_code}')
     assert response.status_code == 200
 
 
@@ -47,11 +67,15 @@ def test_test_returns_correct_body():
     body = ""
     if response.status_code == 200:
         body = response.content.decode("utf-8")
+    if "Hello world!" not in body:
+        logging.error(f'The endpoint test returned wrong body: {body}')
     assert "Hello world!" in body
 
 
 def test_test_name_returns_200():
     response = requests.get(url + "/test/john")
+    if response.status_code != 200:
+        logging.error(f'The endpoint test name returned wrong status code: {response.status_code}')
     assert response.status_code == 200
 
 
@@ -60,6 +84,8 @@ def test_test_name_returns_correct_body():
     body = ""
     if response.status_code == 200:
         body = response.content.decode("utf-8")
+    if 'Hello john!' not in body:
+        logging.error(f'The endpoint test name returned wrong body: {body}')
     assert "Hello john!" in body
 
 
@@ -69,7 +95,8 @@ def test_metainfo_primary_key_returns_200():
         response = requests.get(url + "/metainfo/" + table + "/primarykey")
         if response.status_code != 200:
             errors.append("The endpoint " + url + "/metainfo/" + table + "/primarykey returned status code different then 200.")
-
+    if errors:
+        logging.error(f'For endpoint metainfo/primarykey the following errors for the status code occurred: {errors}')
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
@@ -80,7 +107,8 @@ def test_metainfo_primary_key_returns_correct_body():
         body = json.loads(response.content.decode("utf-8"))
         if body != primary_keys()[table]:
             errors.append("The endpoint " + url + "/metainfo/" + table + "/primarykey returned wrong body.")
-
+    if errors:
+        logging.error(f'For endpoint metainfo/primarykey the following errors for the body occurred: {errors}')
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
@@ -91,7 +119,8 @@ def test_metainfo_primary_key_returns_correct_schema():
         body = json.loads(response.content.decode("utf-8"))
         if not check(metainfo_primarykey_columns_schema(), body):
             errors.append("The endpoint " + url + "/metainfo/" + table + "/primarykey doesn't match the schema.")
-
+    if errors:
+        logging.error(f'For endpoint metainfo/primarykey the following errors for the schema occurred: {errors}')
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
@@ -101,7 +130,8 @@ def test_metainfo_columns_returns_200():
         response = requests.get(url + "/metainfo/" + table + "/columns")
         if response.status_code != 200:
             errors.append("The endpoint " + url + "/metainfo/" + table + "/columns returned status code different then 200.")
-
+    if errors:
+        logging.error(f'For endpoint metainfo/columns the following errors for the status code occurred: {errors}')
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
@@ -112,7 +142,8 @@ def test_metainfo_columns_returns_correct_body():
         body = json.loads(response.content.decode("utf-8"))
         if body != columns()[table]:
             errors.append("The endpoint " + url + "/metainfo/" + table + "/columns returned wrong body.")
-
+    if errors:
+        logging.error(f'For endpoint metainfo/columns the following errors for the body occurred: {errors}')
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
@@ -123,7 +154,8 @@ def test_metainfo_columns_returns_correct_schema():
         body = json.loads(response.content.decode("utf-8"))
         if not check(metainfo_primarykey_columns_schema(), body):
             errors.append("The endpoint " + url + "/metainfo/" + table + "/columns doesn't match the schema.")
-
+    if errors:
+        logging.error(f'For endpoint metainfo/columns the following errors for the schema occurred: {errors}')
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
@@ -133,7 +165,8 @@ def test_metainfo_indexes_returns_200():
         response = requests.get(url + "/metainfo/" + table + "/indexes")
         if response.status_code != 200:
             errors.append("The endpoint " + url + "/metainfo/" + table + "/indexes returned status code different then 200.")
-
+    if errors:
+        logging.error(f'For endpoint metainfo/indexes the following errors for the status code occurred: {errors}')
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
@@ -144,7 +177,8 @@ def test_metainfo_indexes_returns_correct_body():
         body = json.loads(response.content.decode("utf-8"))
         if body != indexes()[table]:
             errors.append("The endpoint " + url + "/metainfo/" + table + "/indexes returned wrong body.")
-
+    if errors:
+        logging.error(f'For endpoint metainfo/indexes the following errors for the body occurred: {errors}')
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
 
 
@@ -155,5 +189,6 @@ def test_metainfo_indexes_returns_correct_schema():
         body = json.loads(response.content.decode("utf-8"))
         if not check(metainfo_indexes_schema(), body):
             errors.append("The endpoint " + url + "/metainfo/" + table + "/indexes doesn't match the schema.")
-
+    if errors:
+        logging.error(f'For endpoint metainfo/indexes the following errors for the schema occurred: {errors}')
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))
